c87765d96aec07dea76722ad9b75df37a63d4181
Khushal Gupta <khushal531@gmail.com>
2018-09-30 23:43:00 +0530

    initial code change

--- ConsoleGame.java
+++ ConsoleGame.java
@@ -1,19 +1,3 @@
-/* 
- * Copyright (C) 2014 Vasilis Vryniotis <bbriniotis at datumbox.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package com.datumbox.opensource;
 
 import com.datumbox.opensource.ai.AIsolver;
@@ -26,8 +10,6 @@ import java.util.Scanner;
 
 /**
  * The main class of the Game.
- * 
- * @author Vasilis Vryniotis <bbriniotis at datumbox.com>
  */
 public class ConsoleGame {
 
@@ -42,7 +24,6 @@ public class ConsoleGame {
         System.out.println("The 2048 Game in JAVA!");
         System.out.println("======================");
         System.out.println();
-        System.out.println("The Game is developed by Vasilis Vryniotis, visit Datumbox.com for more information. This implementation is based on the ideas and projects of Gabriele Cirulli and Matt Overlan.");
         while(true) {
             printMenu();
             int choice;
